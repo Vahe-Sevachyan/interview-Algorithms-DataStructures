2976e5d70cc88bba9473fe8ac7d19e08d67edef2
Vahe Sevachyan <vscode13@gmail.com>
2023-07-18 20:37:43 -0700

    added new string challenge

---
+++ String Methods/index.js
@@ -0,0 +1,21 @@
+// 1.create a function to find the number of words in a string.
+// The function should take a string as an argument.
+// The function should return the number of words in the string.
+// 2. create a function to check if a string is longer than a certain number of characters
+// The function should take two arguments: the first argument is the string, and the second argument is the number of characters.
+// The function should return true if the string is longer than the number of characters, and false if it is not.
+
+function stringLength(str, num) {
+  if (str.length > num) {
+    return true;
+  }
+  return false;
+}
+stringLength("newWord", 9);
+
+function findWords(str) {
+  const newStr = str.split(" ");
+  console.log(newStr);
+}
+
+findWords("new words");
