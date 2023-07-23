c7bd734425fc408c368239db6d3634edb2ea4a9e
Vahe Sevachyan <vscode13@gmail.com>
2023-07-22 17:03:10 -0700

    added second soultion for whitespace challenge

--- String Methods/whitespace.ts
+++ String Methods/whitespace.ts
@@ -1,14 +1,22 @@
+//option 1
+
 function checkWhiteSpace(str: string) {
   const trimmedStr = str.trim();
-  if (trimmedStr.length === str.length) {
+  if (trimmedStr.length !== str.length) {
     return true;
-  } else {
-    false;
   }
+  false;
 }
 
-checkWhiteSpace("i have white space");
+checkWhiteSpace(" i have white space");
 
-//||
+// ||
 
 console.log(checkWhiteSpace("i have white space"));
+
+// option 2
+
+function hasWhiteSpace(str: string) {
+  return str.indexOf(" ") !== -1;
+}
+console.log(hasWhiteSpace("  i-have_whiteSpace    "));
