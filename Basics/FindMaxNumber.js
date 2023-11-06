fe420363ab9df9117650a3c04613fff740494694
Vahe Sevachyan <vscode13@gmail.com>
2023-11-05 20:23:21 -0800

    completed maxNum challange using the filter method

--- Basics/FindMaxNumber.js
+++ Basics/FindMaxNumber.js
@@ -8,3 +8,15 @@ function findMaxNumber(maxNumber) {
   return maxNum;
 }
 console.log(findMaxNumber([1, 2, 4, 5, 6, 20, 34]));
+
+function maxNumber(maximumNum) {
+  let maxNum = 0;
+  maximumNum.filter((num) => {
+    if (num > maxNum) {
+      maxNum = num;
+    }
+  });
+  return maxNum;
+}
+
+console.log(maxNumber([1, 2, 4, 5, 6, 20]));
