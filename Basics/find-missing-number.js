c4a2a305a43b603c820182c2e66c07b1c0ea271a
Vahe Sevachyan <vscode13@gmail.com>
2023-12-17 18:35:42 -0800

    completed missing number challege

---
+++ Basics/find-missing-number.js
@@ -0,0 +1,12 @@
+function findMissingNumber(arr) {
+  if (arr.length === 0) return 1;
+  const n = arr.length + 1;
+  const expectedSum = (n * (n + 1)) / 2;
+
+  let actualSum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    actualSum += arr[i];
+  }
+  return expectedSum - actualSum;
+}
+findMissingNumber([10, 8, 6, 7, 5, 4, 2, 3, 1]);
