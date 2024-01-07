887bf6fd3c3fbff9195b6657d24041d8bbb6bef1
Vahe Sevachyan <79175821+Vahe-Sevachyan@users.noreply.github.com>
2024-01-06 16:22:12 -0800

    completed longestWord challenge

---
+++ Basics/longestWord.js
@@ -0,0 +1,12 @@
+function longestWord(sentence) {
+  const words = sentence.split(' ');
+  let longest = '';
+  
+  for (const word of words) {
+    if (word.length > longest.length) {
+      longest = word;
+    }
+  }
+  
+  return longest;
+}
