53488b6b4a212a53ade9b296810a2ef934810aed
Vahe Sevachyan <vscode13@gmail.com>
2024-01-23 21:44:37 -0800

    completed remove vowels challenge

--- Basics/BasicChallenge.js
+++ Basics/BasicChallenge.js
@@ -2,3 +2,8 @@
 function findMax(arr) {
   return Math.max(...arr);
 }
+//write a function that removes vowels from a given string
+
+function removeVowels(str) {
+  return str.replace(/[aeiou]/gi, "");
+}
